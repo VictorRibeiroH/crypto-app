01bcebd1bfd3e43967c7e718c3356dcd90fd7374
VictorRibeiroH <contato.victoribeiro@gmail.com>
2022-08-11 16:07:01 -0300

    header

--- src/components/CoinItem/index.jsx
+++ src/components/CoinItem/index.jsx
@@ -52,12 +52,12 @@ const CoinItem = ({ marketCoin }) => {
           </View>
           <Text style={styles.text}>{symbol.toUpperCase()}</Text>
           <AntDesign
-            name="caretdown"
+            name={price_change_percentage_24h < 0 ? "caretdown" : "caretup"}
             size={12}
             color={percentageColor}
             style={{ alignSelf: "center", marginRight: 5 }}
           />
-          <Text style={styles.text}>
+          <Text style={{ color: percentageColor }}>
             {price_change_percentage_24h.toFixed(2)}
           </Text>
         </View>
