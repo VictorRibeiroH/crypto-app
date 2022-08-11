6e09b64f8468a73f06ed8c19c517b0c137c0324d
VictorRibeiroH <contato.victoribeiro@gmail.com>
2022-08-11 14:14:33 -0300

    atualizando cor das porcentagens

--- src/components/CoinItem/index.jsx
+++ src/components/CoinItem/index.jsx
@@ -3,13 +3,40 @@ import { Text, View, Image } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import styles from "./styles";
 
-const CoinItem = ({ name }) => {
+const CoinItem = ({ marketCoin }) => {
+  const {
+    name,
+    current_price,
+    market_cap_rank,
+    price_change_percentage_24h,
+    symbol,
+    market_cap,
+    image,
+  } = marketCoin;
+
+  const normalizeMarketCap = (marketCap) => {
+    if (marketCap > 1e12) {
+      return `${(marketCap / 1e12).toFixed(3)} T`;
+    }
+    if (marketCap > 1e9) {
+      return `${(marketCap / 1e9).toFixed(3)} B`;
+    }
+    if (marketCap > 1e6) {
+      return `${(marketCap / 1e6).toFixed(3)} M`;
+    }
+    if (marketCap > 1e3) {
+      return `${(marketCap / 1e3).toFixed(3)} K`;
+    }
+    return marketCap;
+  };
+
+  const percentageColor =
+    price_change_percentage_24h < 0 ? "#ea3943" : "#16c784";
+
   return (
     <View style={styles.coinContainer}>
       <Image
-        source={{
-          uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
-        }}
+        source={{ uri: image }}
         style={{
           height: 30,
           width: 30,
@@ -18,24 +45,28 @@ const CoinItem = ({ name }) => {
         }}
       />
       <View>
-        <Text style={styles.title}>Bitcoin</Text>
+        <Text style={styles.title}>{name}</Text>
         <View style={{ flexDirection: "row" }}>
           <View style={styles.rankContainer}>
-            <Text style={styles.rank}>1</Text>
+            <Text style={styles.rank}>{market_cap_rank}</Text>
           </View>
-          <Text style={styles.text}>BTC</Text>
+          <Text style={styles.text}>{symbol.toUpperCase()}</Text>
           <AntDesign
             name="caretdown"
             size={12}
-            color="white"
+            color={percentageColor}
             style={{ alignSelf: "center", marginRight: 5 }}
           />
-          <Text style={styles.text}>0,63</Text>
+          <Text style={styles.text}>
+            {price_change_percentage_24h.toFixed(2)}
+          </Text>
         </View>
       </View>
-      <View style={{ marginLeft: "auto" }}>
-        <Text style={styles.title}>56265.09</Text>
-        <Text style={styles.text}>MGap 1.076 T</Text>
+      <View style={{ marginLeft: "auto", alignItems: "flex-end" }}>
+        <Text style={styles.title}>{current_price}</Text>
+        <Text style={{ color: "white" }}>
+          MCap {normalizeMarketCap(market_cap)}
+        </Text>
       </View>
     </View>
   );
